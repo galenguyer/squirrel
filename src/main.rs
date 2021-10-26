d6f5a8444013d1816259f1f3336885820d4d14e3
Galen Guyer <galen@galenguyer.com>
2021-10-25 20:56:57 -0400

    watch dump1090 directory for move_to event

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::path::PathBuf;
+
+use inotify::{Inotify, WatchMask};
+
 fn main() {
-    println!("Hello, world!");
+    let mut inotify = Inotify::init().expect("Failed to initialize inotify");
+
+    let watch_dir = PathBuf::from("/run/dump1090-fa/");
+
+    inotify
+        .add_watch(watch_dir, WatchMask::MOVED_TO)
+        .expect("Failed to add watch to /run/dump1090-fa/");
+
+    println!("watches set up");
+
+    let mut buffer = [0u8; 4096];
+    loop {
+        let events = inotify
+            .read_events_blocking(&mut buffer)
+            .expect("Failed to read inotify events");
+        for event in events {
+            println!("Event on {:?}", event.name);
+        }
+    }
 }
