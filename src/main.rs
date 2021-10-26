e33c0c9f79282f5e1360b50c4942ad3bf22818e6
Galen Guyer <galen@galenguyer.com>
2021-10-26 12:11:59 -0400

    deserialize now field from aircraft file

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
-use std::path::PathBuf;
-
+use std::{fs::read_to_string, path::PathBuf};
 use inotify::{Inotify, WatchMask};
+
+mod aircraft;
 
 fn main() {
     let mut inotify = Inotify::init().expect("Failed to initialize inotify");
 
     let watch_dir = PathBuf::from("/run/dump1090-fa/");
+    let aircraft_file = PathBuf::from("/run/dump1090-fa/aircraft.json");
 
     inotify
         .add_watch(watch_dir, WatchMask::MOVED_TO)
@@ -20,7 +22,13 @@ fn main() {
             match event.name {
                 Some(path) => {
                     if path.eq("aircraft.json") {
-                        println!("aircraft.json updated")
+                        match read_to_string(&aircraft_file) {
+                            Ok(contents) => {
+                                let file_contents: aircraft::AircraftFile = serde_json::from_str(&contents).unwrap();
+                                println!("now: {}", file_contents.now);
+                            }
+                            Err(_) => {}
+                        }
                     }
                 }
                 _ => {}
